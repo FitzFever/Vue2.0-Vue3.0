88a7e8f67e9ab67059e44d0f89d524abef33a993
FitzFever <63761608+FitzFever@users.noreply.github.com>
2022-05-19 17:16:27 +0800

    TodoList

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    <School></School>
-    <Student></Student>
-  </div>
+
 </template>
 
 <script>
-import School from './components/School.vue'
-import Student from './components/Student.vue'
+
 export default {
-  components: { School, Student },
+  components: {  },
   name: 'App'
 }
 </script>
